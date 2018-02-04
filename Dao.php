0e190c96a1a3f0afaa5297c62fcbf3c0584cfbfa
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-03 17:25:51 -0700

    fixed user info for Dao.php

--- Dao.php
+++ Dao.php
@@ -1,9 +1,9 @@
 <?php
 class Dao {
-  private $host = "us-cdbr-east-04.cleardb.com";
-  private $db = "heroku_f3d6b64b4b5dc57";
-  private $user = "be7813156b6434";
-  private $pass = "b7cb13a4";
+  private $host = "us-cdbr-iron-east-05.cleardb.net";
+  private $db = "heroku_b9b1f4a7bec7469";
+  private $user = "b187b7773eec73";
+  private $pass = "dd41374f";
   public function getConnection () {
     return
       new PDO("mysql:host={$this->host};dbname={$this->db}", "{$this->user}", "{$this->pass}")
