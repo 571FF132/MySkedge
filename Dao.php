183e82ee99a741ad69919a75863f108607aa1fbe
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-14 18:34:16 -0700

    doing some DAO

--- Dao.php
+++ Dao.php
@@ -4,9 +4,18 @@ class Dao {
   private $db = "heroku_b9b1f4a7bec7469";
   private $user = "b187b7773eec73";
   private $pass = "dd41374f";
-  public function getConnection () {
-    return
-      new PDO("mysql:host={$this->host};dbname={$this->db}", "{$this->user}", "{$this->pass}")
+
+  private function getConnection () {
+    try {
+      return
+        new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass)
+    } catch (Exception $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+
+  public function Connect(){
+   $conn = $this->getConnection();
   }
  
 }
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("Dao.php");
+connect();
 require_once("header.php");
 require_once("body.php");
 require_once("footer.php");
