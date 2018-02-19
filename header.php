b0f4a38d2cf1e7ce6fd47fdd169c5fa3ba748224
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-18 22:18:51 -0700

    header and css change

--- header.php
+++ header.php
@@ -4,21 +4,21 @@
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-    <div class="navigation-bar">
+    <div id="navigation">
     <ul class="navigation-bar">
-      <div class="navigation-item" id="navigation-home">
+    /*  <div class="navigation-item" id="navigation-home"> */
       <li>
         <a href="index.php">
           <img src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
         </a>
       </li>
-      </div>     
-      <div class="navigation-item">
+   /*   </div> */    
+   /*   <div class="navigation-item"> */
       <li>
         <a href="login.php">Login/Sign Up
         </a>   
       </li>
-      </div>
+   /*   </div> */
     <ul>
     </div>
 
