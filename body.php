e8b4d073e4c6f65fb607c475ba97a1e1bf0a0c6a
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-18 22:10:58 -0700

    some css style

--- body.php
+++ body.php
@@ -1,9 +1,15 @@
-<div class="img" id="carousel">
-  <img src="/images/barbershop.jpg">
-  <img src="/images/nailsalon.jpg">
-  <img src="/images/calendar.png">
+<div class="img" class="carousel">
+  <img class="main-carousel" src="/images/barbershop.jpg">
+  <img class="main-carousel" src="/images/nailsalon.jpg">
+  <img class="main-carousel" src="/images/calendar.png">
 </div>  
 
-<div class="horizontal section">
-  <a href="login.php">Login/Sign Up</a>
+<div class="about">
+  <p> With MySkedge you can <div class="about-detail"> schedule and manage appointments with customers and clients from anywhere. </div> Customers and clients will have an easy and convenient way to schedule appointments with you.
+
+
+<div class="call-to-action">
+  <a href="login.php">Sign up now! It's 100% NOT free!</a>
+</div>
+  </p>
 </div>
---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<?php
+require_once("header.php");
+?>
+
+
+
+
+<div><h1>MY SKEDGE PIRU LOVE</h1></div>
+
+
+<?php
+require_once("footer.php");
+?>
+
