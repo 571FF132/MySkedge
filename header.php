0e578c3828c690425a2a15936b4d18bf1c0bc933
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-18 21:27:36 -0700

    added nav bar

--- header.php
+++ header.php
@@ -4,5 +4,21 @@
     <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-
+    <div class="navigation-bar">
+    <ul class="navigation-bar">
+      <div class="navigation-item" id="navigation-home">
+      <li>
+        <a href="index.php">
+          <img src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
+        </a>
+      </li>
+      </div>     
+      <div class="navigation-item">
+      <li>
+        <a href="login.php">Login/Sign Up
+        </a>   
+      </li>
+      </div>
+    <ul>
+    </div>
 
