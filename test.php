8bd3031d747d611b604d165d29b72b93cc426075
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-21 18:31:22 -0700

    more css styling

---
+++ test.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <title>MySkedge</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+
+
+<header>
+    <div class="container">
+      <h1 class="logo"></h1>
+
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+
+<body>
+    <div id="navigation">
+    <ul class="navigation-bar">
+      <li class="navigation-item" id="navigation-home">
+        <a href="index.php">
+          <img id="brand" src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
+        </a>
+      </li>
+      <li class="navigation-item">
+        <a href="login.php">Login/Sign Up
+        </a>
+      </li>
+    </div>
+
+</body>
+</html>
