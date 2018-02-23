52013d2299381417148fd323d455f3d8f7eee048
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-02-22 17:14:11 -0700

    added some js and carousel test

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <title>MySkedge</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/teststyle.css">
   </head>
 
 
@@ -34,6 +34,56 @@
         </a>
       </li>
     </div>
+
+<div class="wrap">
+  <div class="wrap2">
+    <div class="group">
+      <input type="radio" name="test" id="0" value="0">
+      <label for="4" class="previous">&lt;</label>
+      <label for="1" class="next">&gt;</label>
+      <div class="content">
+        <p>panel #0</p>
+        <img src="http://i.stack.imgur.com/R5yzx.jpg" width="200" height="286">
+      </div>
+    </div>
+    <div class="group">
+      <input type="radio" name="test" id="1" value="1">
+      <label for="0" class="previous">&lt;</label>
+      <label for="2" class="next">&gt;</label>
+      <div class="content">
+        <p>panel #1</p>
+        <img src="http://i.stack.imgur.com/k0Hsd.jpg" width="200" height="139">
+      </div>
+    </div>
+    <div class="group">
+      <input type="radio" name="test" id="2" value="2">
+      <label for="1" class="previous">&lt;</label>
+      <label for="3" class="next">&gt;</label>
+      <div class="content">
+        <p>panel #2</p>
+        <img src="http://i.stack.imgur.com/Hhl9H.jpg" width="140" height="200">
+      </div>
+    </div>
+    <div class="group">
+      <input type="radio" name="test" id="3" value="3" checked="">
+      <label for="2" class="previous">&lt;</label>
+      <label for="4" class="next">&gt;</label>
+      <div class="content">
+        <p>panel #3</p>
+        <img src="http://i.stack.imgur.com/r1AyN.jpg" width="200" height="287">
+      </div>
+    </div>
+    <div class="group">
+      <input type="radio" name="test" id="4" value="4">
+      <label for="3" class="previous">&lt;</label>
+      <label for="0" class="next">&gt;</label>
+      <div class="content">
+        <p>panel #4</p>
+        <img src="http://i.stack.imgur.com/EHHsa.jpg" width="96" height="139">
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
 </html>
