921edd7b3fd9f99307f269453cefb190f0f854a2
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-15 16:28:18 -0600

    trying to get launched again

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 /*require_once("Dao.php");
 $dao = new Dao();
 $dao->connect();*/
+include_once("index.html");
 require_once("header.php");
 require_once("body.php");
 require_once("footer.php");
