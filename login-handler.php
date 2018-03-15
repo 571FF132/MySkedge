ecc034d4769f8a4f80ee4f49ea3ccaaaeb30fdfa
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-15 17:36:31 -0600

    message

--- login-handler.php
+++ login-handler.php
@@ -3,15 +3,15 @@
 session_start();
 
 // For simplification Lets pretend I got these login credentials from an SQL table.
-if ("ckennington@gmail.com" == $_POST["email"] &&
-    "lollipop" == $_POST["password"]) {
+if ("123" == $_POST["username"] &&
+    "123" == $_POST["password"]) {
   $_SESSION["access_granted"] = true;
   header("Location:granted.php");
 
 } else {
   $status = "Invalid username or password";
   $_SESSION["status"] = $status;
-  $_SESSION["email_preset"] = $_POST["email"];
+  $_SESSION["email_preset"] = $_POST["username"];
   $_SESSION["access_granted"] = false;
 
   header("Location:login.php");
