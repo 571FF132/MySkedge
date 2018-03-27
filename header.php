114b7f5363d12a624cb8d323bc9b41e51aeb79d2
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-27 16:39:10 -0600

    trying to clean up header

--- header.php
+++ header.php
@@ -2,15 +2,17 @@
   <head>
     <title>MySkedge</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="
   </head>
   <body>
     <div id="navigation">
-    <ul class="navigation-bar">
-      <li class="navigation-item" id="navigation-home">
-        <a href="index.php">
-          <img id="brand" src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
-        </a>
-      </li>
+    <span class="navigation-item" id="navigation-home">
+      <a href="index.php">
+        <img id="brand" src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
+      </a>
+    </span>
+    <span class ="navigation-item" id="navigation-links>
+    <ul id=navigation-links-list">
       <li class="navigation-item">
 	<a href="login.php">LogIN</a>
       </li>
