319b4e98be8e8716d46154f260001ae96506d93c
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-27 16:43:49 -0600

    header fix

--- header.php
+++ header.php
@@ -1,21 +1,21 @@
 <html>
-  <head>
-    <title>MySkedge</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="
+<head>
+    	<title>MySkedge</title>
+    	<link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
-    <div id="navigation">
-    <span class="navigation-item" id="navigation-home">
-      <a href="index.php">
-        <img id="brand" src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
-      </a>
-    </span>
-    <span class ="navigation-item" id="navigation-links>
-    <ul id=navigation-links-list">
-      <li class="navigation-item">
-	<a href="login.php">LogIN</a>
-      </li>
-    </ul>
-    </div>
+    	<div id="navigation">
+    	<span class="navigation-item" id="navigation-home">
+      		<a href="index.php">
+       			<img id="brand" src="/images/MySkedgeLogo256x256.png" alt="MySkedge.com">
+  		</a>
+    	</span>
+    	<span class ="navigation-item" id="navigation-links>
+    	<ul id=navigation-links-list">
+      		<li class="navigation-item">
+			<a href="login.php">LogIN</a>
+      		</li>
+    	</ul>
+    	</span> 
+    	</div>
 
