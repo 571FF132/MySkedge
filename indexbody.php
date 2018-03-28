afe454a270bf1eab49de8fe1be7f90c2678828c2
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-28 17:10:54 -0600

    taking out indexbody

--- indexbody.php
+++ indexbody.php
@@ -4,8 +4,8 @@
   <img class="main-carousel" src="/images/nailsalon.jpg">
   <img class="main-carousel" src="/images/calendar.png">
   <span class="about">
-  </span>
     <p> With MySkedge you can schedule and manage appointments with customers and clients from anywhere. Customers and clients will have an easy and convenient way to schedule appointments with you.</p>
+  </span>
   <span class="carousel-login-button>
     <a href="login.php">Sign up now! It's 100% NOT free!</a>
   </span>
