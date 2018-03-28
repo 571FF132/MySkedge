1deea08429b68296075f189832fc74d96141a388
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-28 17:04:39 -0600

    trying to compartamentalize

--- indexbody.php
+++ indexbody.php
@@ -5,7 +5,7 @@
   <img class="main-carousel" src="/images/calendar.png">
   <span class="about">
   </span>
-    <p> With MySkedge you can schedule and manage appointments with customers and clients from anywhere. Customers and clients will have an easy and convenient way to schedule appointments with you.
+    <p> With MySkedge you can schedule and manage appointments with customers and clients from anywhere. Customers and clients will have an easy and convenient way to schedule appointments with you.</p>
   <span class="carousel-login-button>
     <a href="login.php">Sign up now! It's 100% NOT free!</a>
   </span>
