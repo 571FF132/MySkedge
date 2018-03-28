312803b24cb5302ca240b98bf06c28e041e4e4cd
Justin Stiffler <JustinStiffler@u.boisestate.edu>
2018-03-27 20:41:52 -0600

    adding forms to dashboard

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@ require_once("header.php");
 
 
 
-<div><h1>MY SKEDGE PIRU LOVE</h1></div>
+<div><h1>MY SKEDGE</h1></div>
 
 <ul>
   <?php foreach ($appointments as $appointment) {
@@ -20,6 +20,40 @@ require_once("header.php");
   echo "<li>" . $rid . "</li>";
   ?>
 </ul>
+
+<div class="appointment-form-wrapper">
+	<form action="appointment-handler.php" class="login-form" method="POST">
+		<div class="form-area">
+			Business:
+			<select>
+  				<option value="business1">business1</option>
+  				<option value="business2">barber2</option>
+  				<option value="business3">nailsalon3</option>
+  				<option value="business4">mechanic4</option>
+			</select>
+		</div>
+		<div class="form-area">
+			Employee:
+			<select>	
+				<option value="any">No preference</option>
+			</select>
+		</div>
+		<div class="form-area">
+			Appointment Start:
+			<input name="appointment-start-date" type="date">
+			<input name="appointment-start-time" type="time">
+		</div>
+		<div class="form-area">
+                        Appointment End:
+                        <input name="appointment-end-date" type="date">
+                        <input name="appointment-end-time" type="time">
+                </div>
+		<div class="form-area">
+                        <input name="appointment-submit-button" type="submit" value="Add Appointment">
+                </div>
+
+	</form>
+</div>
 
 
 <?php
--- login.php
+++ login.php
@@ -25,17 +25,17 @@ require_once("header.php");
 <div id="login">
 <h1>Login</h1>
 <form action="login-handler.php" class="login-form"  method ="POST">
-  <div class ="form area">
+  <div class ="form-area">
     Username:
     <input placeholder="username here" name="username" class="username-login-input" type="text">
   </div>
 
-  <div class ="form area">
+  <div class ="form-area">
     Password:
     <input name="password" class="password-login-input" type="password">
   </div>
 
-  <div class ="form area">
+  <div class ="form-area">
     <input name="loginButton" type="submit" value="Sign In">
   </div>
 </form>
